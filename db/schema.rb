7709064b5ae91faa8221f3db8bd517129f737686
Joey <jjcsull@gmail.com>
2021-03-09 16:28:24 +1100

    Added user_id column to products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_143054) do
+ActiveRecord::Schema.define(version: 2021_03_09_052341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,9 @@ ActiveRecord::Schema.define(version: 2021_03_08_143054) do
     t.integer "condition"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,4 +53,5 @@ ActiveRecord::Schema.define(version: 2021_03_08_143054) do
   end
 
   add_foreign_key "products", "categories"
+  add_foreign_key "products", "users"
 end
