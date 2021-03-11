1a274f47f7dbfb7bcde6614b206a92e8a7602de2
Joey <jjcsull@gmail.com>
2021-03-11 15:05:22 +1100

    Created user profile model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_052341) do
+ActiveRecord::Schema.define(version: 2021_03_11_040124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,15 @@ ActiveRecord::Schema.define(version: 2021_03_09_052341) do
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
+  create_table "user_profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_user_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -54,4 +63,5 @@ ActiveRecord::Schema.define(version: 2021_03_09_052341) do
 
   add_foreign_key "products", "categories"
   add_foreign_key "products", "users"
+  add_foreign_key "user_profiles", "users"
 end
