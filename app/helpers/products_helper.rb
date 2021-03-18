e17ce288cc776c77d1ecd0725304ba090ab64b12
Joey <jjcsull@gmail.com>
2021-03-18 15:35:56 +1100

    Fixed config for heroku

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,5 @@
 module ProductsHelper
+    def format_condition(condition)
+        condition.split("_").map{|word| word.capitalize}.join(" ")
+    end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
   belongs_to :user
-  enum condition: {damaged: 0, heavilyused: 1, used: 2, likenew: 3, brandnew: 4}
+  has_one :purchase, dependent: :destroy
+  enum condition: {damaged: 0, heavily_used: 1, used: 2, like_new: 3, brand_new: 4}
   has_one_attached :picture
 end
